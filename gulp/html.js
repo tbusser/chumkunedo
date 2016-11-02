a747768b47ab46a553cddef36cab51d01fa3e95c
Thijs Busser <tbusser@gmail.com>
2016-11-02 20:22:00 +0100

    Add Gulp tasks
    Add the tasks needed to develop and deploy the project.

---
+++ gulp/html.js
@@ -0,0 +1,23 @@
+module.exports = function(gulp, config, plugins) {
+	'use strict';
+
+	gulp.task('build:asset:html', function() {
+		var paths = config[config.environment];
+
+		return gulp.src(config.src.html + '*.html')
+			.pipe(plugins.fileInclude({
+				prefix: '@@',
+				basePath: '@file',
+				context: {
+					deploy: config.deploy
+				}
+			}))
+			.pipe(plugins.if(config.deploy, plugins.stripComments()))
+			.pipe(plugins.if(config.deploy, plugins.sitemap(config.sitemap)))
+			.pipe(gulp.dest(paths.html));
+	});
+
+	gulp.task('watch:html', function() {
+		gulp.watch(config.src.html + '**/*.htm*', ['build:asset:html']);
+	});
+};
